38d09cb397d5252853b84b52f886ff2b758f0047
gary king <jin.songbo@yunceshi.com>
2020-10-19 15:09:44 +0800

    First commit.

---
+++ src/apollo/client.js
@@ -0,0 +1,34 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/explorer/swapdefi/litchiswap'
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+export const healthClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/index-node/graphql'
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+export const v1Client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/explorer/swapdefi/litchiswapv1'
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true
+})
+
+export const blockClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks'
+  }),
+  cache: new InMemoryCache()
+})
